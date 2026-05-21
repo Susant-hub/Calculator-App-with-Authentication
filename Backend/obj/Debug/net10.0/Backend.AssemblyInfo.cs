dd71f5513009b17a19a1bb0ceea288aa87286bac
Tech_sus <subedisusant632@gmail.com>
2026-05-21 17:35:04 +0545

    updated title of the page

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e76568b6df7d3904d434ec479d18d0811165da70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42afb4b4d25acc28c0c445c8d97c993c314fc03f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
